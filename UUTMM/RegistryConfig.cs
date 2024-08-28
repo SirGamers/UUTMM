603b438317e9cfdbafe3d6480052777505c08682
SirGamers <jdflury2016@gmail.com>
2024-08-28 19:36:30 -0400

    squash

---
+++ UUTMM/RegistryConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+
+namespace UUTMM
+{
+    public static class RegistryConfig
+    {
+        public static bool InstallGBHandler()
+        {
+            string AppPath = $"{Global.assemblyLocation}{Global.s}UUTMM.exe";
+            string protocolName = $"uutmm";
+            try
+            {
+                var reg = Registry.CurrentUser.CreateSubKey(@"Software\Classes\UUTMM");
+                reg.SetValue("", $"URL:{protocolName}");
+                reg.SetValue("URL Protocol", "");
+                reg = reg.CreateSubKey(@"shell\open\command");
+                reg.SetValue("", $"\"{AppPath}\" -download \"%1\"");
+                reg.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
